c126f238a77ee1e632ea03a299664b8f959f8b15
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2026-03-12 00:07:49 +0530

    fixes the branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("font-sans", figtree.variable)}
-    >
+    <html lang="en" className={cn("font-sans", figtree.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
